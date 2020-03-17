624febeca9d2f893b55ee5907641a36b2ac34770
Jeel Bharatkumar Gandhi <jgandhi@isimtl.local>
2020-03-17 19:06:34 -0400

    register api created

--- application/controllers/api/signup.php
+++ application/controllers/api/signup.php
@@ -15,21 +15,31 @@ class login extends REST_Controller {
         //load user model
         $this->load->model('user_model');
     }
-    public function index_post($id=0) {
-        //returns all rows if the id parameter doesn't exist,
-        //otherwise single row will be returned
-        if($id==0){
-            $users= $this->user_model->get_users();
-        }
-        else{
-            $users = $this->user_model->get_user($id);
-        }
-        //check if the user data exists
-        if(!empty($users)){
-            //set the response and exit
-         
-             $this->response($users);
-        }else{
+	public function index_post() {
+        //returns login authentication result if login succesful then send user detail
+		//otherwise single row will be returned
+		
+	 $this->form_validation->set_rules('name', 'First Name', 'required|alpha');
+	 $this->form_validation->set_rules('name', 'Last Name', 'required|alpha');
+	 $this->form_validation->set_rules('email', 'Email', 'trim|required|valid_email|xss_clean');
+	 $this->form_validation->set_rules('password', 'Password', 'required|min_length[8]|max_length[16]');
+	 $this->form_validation->set_rules('contact_no', 'Contact Number', 'required');
+	 $this->form_validation->set_rules('reg[dob]', 'Date of birth', 'regex_match[(0[1-9]|1[0-9]|2[0-9]|3(0|1))-(0[1-9]|1[0-2])-\d{4}]');		
+	 $this->form_validation->set_rules('gender', 'Gender', 'required');
+
+	 if ($this->form_validation->run() == TRUE) {
+
+	 $res = $this->user_model->add_user($data);
+	 if($res)
+	 {
+		 $this->session->set_userdata('user',$res);
+		 if($this->session->userdata('redirectTo'))
+		 {	
+			 $re = $this->session->userdata('redirectTo');
+			 $this->session->unset_userdata('redirectTo');
+			 redirect($re);				
+		 }
+		 else{
             //set the response and exit
             $res=array(
                 'status' => FALSE,
@@ -37,4 +47,7 @@ class login extends REST_Controller {
             );
             $this->response($res);
         }
+			 
+	 }
     }
+  
