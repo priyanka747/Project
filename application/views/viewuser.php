38fae707225e215e236af0bbe195d223f14bf906
Gaganpreet Kaur Gaganpreet Kaur <ggaganpr@isimtl.local>
2020-03-18 18:02:23 -0400

    Created viewsubcategory and viewuser pages.

---
+++ application/views/viewuser.php
@@ -0,0 +1,80 @@
+      <!-- Content -->
+      <div class="content">
+        <!-- Animated -->
+        <div class="animated fadeIn">
+
+          <div class="clearfix"></div>
+          <!-- Orders -->
+          <div class="orders">
+            <div class="row">
+              <div class="col-xl-12">
+                <div class="card">
+                  <div class="card-body">
+                    <h4 class="box-title">List Of All Users  </h4>
+                  </div>
+                  <div class="card-body--">
+                    <div class="table-stats order-table ov-h">
+                      <table class="table ">
+                        <thead>
+                          <tr>
+                            <th>User ID</th>
+                            <th>First Name</th>      
+                            <th>Last Name</th>      
+                            <th>DOB</th>      
+                            <th>Contact</th>                
+                            <th>E-Mail</th>                            
+                            <th>Action</th>
+                          </tr>                              
+                        </thead>
+                        <tbody>
+                          <tr>
+                            <td>1177</td>
+                            <td>Jean</td>
+                            <td>Talon</td>   
+                            <td>1/1/2001</td>    
+                            <td>123456789</td>    
+                            <td>talon.jean@gmail.com</td>             
+                            <td> <button class='btn btn-danger' >delete</button> <button class='btn btn-info' >edit</button></td>
+                          </tr>
+						  <tr>
+                            <td>7542</td>
+                            <td>Ceren</td>
+                            <td>Selevian</td>   
+                            <td>15/4/1991</td>    
+                            <td>145623789</td>    
+                            <td>cerens123@yahoo.com</td>        
+                            <td> <button class='btn btn-danger' >delete</button> <button class='btn btn-info' >edit</button></td>
+                          </tr>
+						  <tr>
+                            <td>8317</td>
+                            <td>Francois</td>
+                            <td>klea</td>   
+                            <td>29/10/1989</td>    
+                            <td>567891234</td>    
+                            <td>kelanfranc@gmail.com</td>       
+                            <td> <button class='btn btn-danger' >delete</button> <button class='btn btn-info' >edit</button></td>
+                          </tr>
+						  <tr>
+                            <td>6576</td>
+                            <td>Sandy</td>
+                            <td>Jakaria</td>   
+                            <td>8/11/2002</td>    
+                            <td>56781232425</td>    
+                            <td>jakaria.jakaria@hotmail.com</td>      
+                            <td> <button class='btn btn-danger' >delete</button> <button class='btn btn-info' >edit</button></td>
+                          </tr>
+
+                        </tbody>
+                      </table>
+                    </div> <!-- /.table-stats -->
+                  </div>
+                </div> <!-- /.card -->
+              </div>  <!-- /.col-lg-8 -->
+
+
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- /.content -->
+      <div class="clearfix"></div>
