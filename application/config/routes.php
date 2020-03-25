171a6628e06bbc6844573de4914ebdf347d9c952
Dawn Narikkattu Mathews <dnarikka@isi-mtl.com>
2020-03-24 20:36:34 -0400

    Created an api to get orders by status. userid and status is passed in the uri.

--- application/config/routes.php
+++ application/config/routes.php
@@ -23,5 +23,26 @@ $route['category/delete/(:any)'] = 'categorycontroller/delete/$1';
 
 /*=========================API ROUTES================================*/
 
+//----------------------------------------------------------------
+
+//http://localhost/Project/api/Getorderbyuser
+//$route['createorder']['GET'] = 'createorder/index_get';//initial call to getorderbyuser index
+
+//http://localhost/Project/api/Getorderbyuser/2
+/** overrides the above **/
+//$route['createorder/(:num)']['GET'] = 'createorder/index_get/$1';//Changing defaults index
+
+//http://localhost/Project/api/Getorderbyuser/2/2
+/** overrides the above **/
+//$route['createorder/(:num)/(:any)']['GET'] = 'createorder/order/$1/$2';//Changing type,limit,offset
+
+//All routes that are similar, like above that follow the previous, override the preceding one. 
+
+
+/*//http://localhost/Project/api/Getorderbyuser/create
+
+//overrides $route['players/(:any)']
+$route['getorderbyuser/create'] = 'getorderbyuser/index_post';*/
+
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
