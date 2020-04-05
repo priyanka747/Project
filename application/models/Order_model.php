da3d6744ac362be2aa07a6299926dd9bf3d7ab5c
Dawn Narikkattu Mathews <dnarikka@isi-mtl.com>
2020-04-05 06:58:19 -0400

    Order Api Updated

--- application/models/Order_model.php
+++ application/models/Order_model.php
@@ -5,6 +5,67 @@ class Order_model extends CI_Model
 	//orders join order_info
 	//----------------------------------------------------------------------------------
 
+	//get one order
+	function get_order($order_id)
+	{
+			$data=$this->db->select('order_id, user_id, promotion_id, shipping_address_id, mail_address_id, date, shipped_status, order_status, payment_type, total')->from ('orders')->where ('status ','active')->where ('order_id ', $order_id)->order_by('date_create','desc')->get()->result_array();
+			$order_cnt=count($data);
+			//echo $order_cnt;
+			for($i=0;$i<$order_cnt;$i++)
+			{
+				$data[$i]['user']=$this->get_user_details($data[$i]['user_id']);
+			}
+			for($i=0;$i<$order_cnt;$i++)
+			{
+				$data[$i]['promotion']=$this->get_promotion_details($data[$i]['promotion_id']);
+			}
+			for($i=0;$i<$order_cnt;$i++)
+			{
+				$data[$i]['shipping_address']=$this->get_shipping_address($data[$i]['shipping_address_id']);
+			}
+			for($i=0;$i<$order_cnt;$i++)
+			{
+				$data[$i]['mail_address']=$this->get_mailing_address($data[$i]['mail_address_id']);
+			}
+			for($i=0;$i<$order_cnt;$i++)
+			{
+				$data[$i]['products']=$this->get_products($data[$i]['order_id']);
+			}
+			return $data;
+	}
+
+	//--------------------
+	
+	//get all orders ( active )
+	function get_orders(){
+			$data=$this->db->select('order_id, user_id, promotion_id, shipping_address_id, mail_address_id, date, shipped_status, order_status, payment_type, total')->from ('orders')->where ('status ','active')->order_by('date_create','desc')->get()->result_array();
+			$order_cnt=count($data);
+			//echo $order_cnt;
+			for($i=0;$i<$order_cnt;$i++)
+			{
+				$data[$i]['user']=$this->get_user_details($data[$i]['user_id']);
+			}
+			for($i=0;$i<$order_cnt;$i++)
+			{
+				$data[$i]['promotion']=$this->get_promotion_details($data[$i]['promotion_id']);
+			}
+			for($i=0;$i<$order_cnt;$i++)
+			{
+				$data[$i]['shipping_address']=$this->get_shipping_address($data[$i]['shipping_address_id']);
+			}
+			for($i=0;$i<$order_cnt;$i++)
+			{
+				$data[$i]['mail_address']=$this->get_mailing_address($data[$i]['mail_address_id']);
+			}
+			for($i=0;$i<$order_cnt;$i++)
+			{
+				$data[$i]['products']=$this->get_products($data[$i]['order_id']);
+			}
+			return $data;
+
+		}
+
+
 	function get_total()
 	{
 		$this->db->select_sum('total');
@@ -24,8 +85,9 @@ class Order_model extends CI_Model
     $this->db->where('order_status','completed');
     return $this->db->get()->result_array();
 	}
+
 	//get all active orders
-	function get_orders()
+	function get_orders_active()
 	{
 		$this->db->select('*');
 		$this->db->from('orders');
@@ -141,5 +203,61 @@ class Order_model extends CI_Model
 		$this->db->order_by('date_create','desc');
 		return $this->db->get()->result_array();
 	}
+
+
+	//-----------------------------
+
+	function get_user_details($id)
+	{
+		$this->db->select('user_id, email, first_name, last_name, contact, D_O_B, gender');
+		$this->db->from('users');
+		$this->db->where('user_id',$id);
+		$this->db->order_by('created_date','desc');
+		return $this->db->get()->result_array();
+	}
+
+	function get_promotion_details($id)
+	{
+		$this->db->select('promotion_id, code, promotion_decription, amount, expire');
+		$this->db->from('promotion');
+		$this->db->where('promotion_id',$id);
+		$this->db->order_by('entry_date','desc');
+		return $this->db->get()->result_array();
+	}
+
+	function get_shipping_address($id)
+	{
+		$this->db->select('address_id, street, civil_no, city, state, country, postal_code, appartment');
+		$this->db->from('address');
+		$this->db->where('address_id',$id);
+		$this->db->where('address_type','shipping');
+		$this->db->order_by('created_date','desc');
+		return $this->db->get()->result_array();
+	}
+
+	function get_mailing_address($id)
+	{
+		$this->db->select('address_id, street, civil_no, city, state, country, postal_code, appartment');
+		$this->db->from('address');
+		$this->db->where('address_id',$id);
+		$this->db->where('address_type','mail');
+		$this->db->order_by('created_date','desc');
+		return $this->db->get()->result_array();
+	}
+
+	function get_products($order_id){
+ 
+    	$this->db->select('product.product_id, product.product_name, order_info.quantity');
+		$this->db->from('order_info');
+		$this->db->join('product', 'order_info.product_id = product.product_id', 'inner');
+		//$this->db->join('image', 'product_image.image_id = image.image_id', 'inner');  
+		//$this->db->where('product_image.product_id',$product_id);
+		$this->db->where('product.status','active');
+		$this->db->where('order_info.order_id', $order_id);
+		$this->db->order_by('product.date_created','desc');
+		return $this->db->get()->result_array();
+    }
+
+	//--------------------
 }
 ?>
