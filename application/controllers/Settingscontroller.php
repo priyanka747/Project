6b787c882e429ec6c9c629e1385e6d896e9cfdd0
Jeel Bharatkumar Gandhi <jgandhi@isimtl.local>
2020-04-06 17:10:03 -0400

    done

--- application/controllers/Settingscontroller.php
+++ application/controllers/Settingscontroller.php
@@ -201,6 +201,111 @@
             }
 	}
 	
+
+	function update_contact_details(){
+        $fb = $this->security->xss_clean($this->input->post('fl'));
+        $li = $this->security->xss_clean($this->input->post('ll'));
+        $tweeter = $this->security->xss_clean($this->input->post('tl'));
+        $this->form_validation->set_rules('phone1','Contact Number', 'required|numeric');
+        $this->form_validation->set_rules('phone2','Contact Number', 'required|numeric');
+		$this->form_validation->set_rules('phone3','Contact Number', 'required|numeric');
+		$this->form_validation->set_rules('email1','email', 'Email', 'trim|required|valid_email');
+		$this->form_validation->set_rules('email2','email', 'Email', 'trim|required|valid_email');
+
+        if($this->form_validation->run()== TRUE){
+                    $data=array(
+                        array(
+                            'settings_name'=>'fb',
+                            'settings_value'=>$fb,
+                            'modified_by'=>$this->session->userdata('user')[0]['user_id']
+                        ),
+                        array(
+                            'settings_name'=>'li',
+                            'settings_value'=>$li,
+                            'modified_by'=>$this->session->userdata('user')[0]['user_id']
+                        ),
+                        array(
+                            'settings_name'=>'tweeter',
+                            'settings_value'=>$tweeter,
+                            'modified_by'=>$this->session->userdata('user')[0]['user_id']
+                        )
+                    );
+                $res=$this->setting_model->update_settings($data);
+
+                if($res)
+                {
+                    $this->session->set_userdata('success','settings updated successfully.! ');
+                    redirect(base_url('socialmedia'));
+                }
+                else{
+                    $this->session->set_userdata('error','trouble while updating links');
+                    redirect(base_url('socialmedia'));
+                }
+            }else{
+               $data['page'] = 'setting';
+               $data['fl']=$this->setting_model->get_setting_by_settings_name('fb');
+           $data['ll']=$this->setting_model->get_setting_by_settings_name('li');
+           $data['tl']=$this->setting_model->get_setting_by_settings_name('tweeter');
+                $this->load->view('includes/header');
+                $this->load->view('includes/nav',$data);
+                $this->load->view('changesocialmedialinks',$data);
+                $this->load->view('includes/footer');	
+            }
+	}
+
+	function update_company_details(){
+        $fb = $this->security->xss_clean($this->input->post('fl'));
+        $li = $this->security->xss_clean($this->input->post('ll'));
+        $tweeter = $this->security->xss_clean($this->input->post('tl'));
+        $this->form_validation->set_rules('company_name','Company Name','required|alpha');
+        $this->form_validation->set_rules('myfile','Company Logo','validate_image['.$_FILES['myfile'].']');
+		$this->form_validation->set_rules('cpydate','Copy Right Date', 'regex_match[(0[1-9]|1[0-9]|2[0-9]|3(0|1))-(0[1-9]|1[0-2])-\d{4}]');
+		
+
+        if($this->form_validation->run()== TRUE){
+                    $data=array(
+                        array(
+                            'settings_name'=>'fb',
+                            'settings_value'=>$fb,
+                            'modified_by'=>$this->session->userdata('user')[0]['user_id']
+                        ),
+                        array(
+                            'settings_name'=>'li',
+                            'settings_value'=>$li,
+                            'modified_by'=>$this->session->userdata('user')[0]['user_id']
+                        ),
+                        array(
+                            'settings_name'=>'tweeter',
+                            'settings_value'=>$tweeter,
+                            'modified_by'=>$this->session->userdata('user')[0]['user_id']
+                        )
+                    );
+                $res=$this->setting_model->update_settings($data);
+
+                if($res)
+                {
+                    $this->session->set_userdata('success','settings updated successfully.! ');
+                    redirect(base_url('socialmedia'));
+                }
+                else{
+                    $this->session->set_userdata('error','trouble while updating links');
+                    redirect(base_url('socialmedia'));
+                }
+            }else{
+               $data['page'] = 'setting';
+               $data['fl']=$this->setting_model->get_setting_by_settings_name('fb');
+           $data['ll']=$this->setting_model->get_setting_by_settings_name('li');
+           $data['tl']=$this->setting_model->get_setting_by_settings_name('tweeter');
+                $this->load->view('includes/header');
+                $this->load->view('includes/nav',$data);
+                $this->load->view('changesocialmedialinks',$data);
+                $this->load->view('includes/footer');	
+            }
+	}
+
+
+
+
 	function delete($id){
 		if($this->setting_model->is_parent($id)>0){
 			if($this->product_model->update_setting($data)){
