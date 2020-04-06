3394c6160f22910af3f57e439808a6895cc56915
Gagandeep Banger <gbanger@isi-mtl.com>
2020-04-06 16:54:15 -0400

    Modified Sales report and product report view pages.

--- application/views/productreport.php
+++ application/views/productreport.php
@@ -9,10 +9,18 @@
                             <div class="card-body">
                                 <div class="card-left pt-1 float-left">
                                     <h3 class="mb-0 fw-r">
-                                        <span class="currency float-left mr-1">$</span>
-                                        <span class="count">23569</span>
+                                        <span class="count">23</span>
                                     </h3>
-                                    <p class="text-light mt-1 m-0">Revenue</p>
+                                    <p class="text-light mt-1 m-0">Womens Clothes</p>
+									<div class="form-group">
+  <label for="sel1">Select list:</label>
+  <select class="form-control" id="sel1">
+    <option>Shawl</option>
+    <option>2</option>
+    <option>3</option>
+    <option>4</option>
+  </select>
+</div>
                                 </div><!-- /.card-left -->
 
                                 <div class="card-right float-right text-right">
@@ -29,10 +37,19 @@
                             <div class="card-body">
                                 <div class="card-left pt-1 float-left">
                                     <h3 class="mb-0 fw-r">
-                                        <span class="count float-left">85</span>
-                                        <span>%</span>
+                                        <span class="count">66</span>
                                     </h3>
-                                    <p class="text-light mt-1 m-0">Total Orders</p>
+                                    <p class="text-light mt-1 m-0">Mens Clothes</p>
+									
+									<div class="form-group">
+  <label for="sel1">Select list:</label>
+  <select class="form-control" id="sel1">
+    <option>Tshirt</option>
+    <option>2</option>
+    <option>3</option>
+    <option>4</option>
+  </select>
+</div>
                                 </div><!-- /.card-left -->
 
                                 <div class="card-right float-right text-right">
@@ -50,9 +67,19 @@
                             <div class="card-body">
                                 <div class="card-left pt-1 float-left">
                                     <h3 class="mb-0 fw-r">
-                                        <span class="count">6569</span>
+                                        <span class="count">69</span>
                                     </h3>
-                                    <p class="text-light mt-1 m-0">Total clients</p>
+                                    <p class="text-light mt-1 m-0">Kids Clothes</p>
+									
+									<div class="form-group">
+  <label for="sel1">Select list:</label>
+  <select class="form-control" id="sel1">
+    <option>Sweaters</option>
+    <option>2</option>
+    <option>3</option>
+    <option>4</option>
+  </select>
+</div>
                                 </div><!-- /.card-left -->
 
                                 <div class="card-right float-right text-right">
@@ -65,24 +92,6 @@
                     </div>
                     <!--/.col-->
 
-                    <div class="col-sm-6 col-lg-3">
-                        <div class="card text-white bg-flat-color-2">
-                            <div class="card-body">
-                                <div class="card-left pt-1 float-left">
-                                    <h3 class="mb-0 fw-r">
-                                        <span class="count">1490</span>
-                                    </h3>
-                                    <p class="text-light mt-1 m-0">New users</p>
-                                </div><!-- /.card-left -->
-
-                                <div class="card-right float-right text-right">
-                                    <div id="flotLine1" class="flotLine1"></div>
-                                </div><!-- /.card-right -->
-
-                            </div>
-
-                        </div>
-                    </div>
                     </div>
                 <!-- /Widgets -->
                 
@@ -92,7 +101,7 @@
                         <div class="col-lg-12">
                             <div class="card">
                                 <div class="card-body">
-                                    <h4 class="box-title">Recent Orders </h4>
+                                    <h4 class="box-title">Daily product reports </h4>
                                 </div>
                                 <div class="card-body--">
                                     <div class="table-stats">
@@ -100,25 +109,40 @@
                                             <thead>
                                                 <tr>
                                                     <th class="serial">#</th>
-                                                    <th>ORDER ID</th>
-                                                    <th>client name</th>
-                                                    <th>date</th>
-                                                    <th>order status</th>
-                                                    <th>shippment status</th>
+                                                    <th>Day </th>
+                                                    <th>Category</th>
+                                                    <th>Number of product sold</th>
+                                                    <th>total revenue</th>
+                                                   
                                                 </tr>
                                             </thead>         
                                             <tbody>
                                                 <tr>
                                                     <td class="serial">1.</td>
-                                                    <td> #5469 </td>
-                                                    <td>  <span class="client name">Louis Stanley</span> </td>
-                                                    <td>  <span class="date">2020-03-19 14:20:02</span> </td>
-                                                    <td><span class="order status"> <span class="badge badge-pending">pennding</span></span> </td>
-                                                    <td>
-                                                        <span class="badge badge-pending">not shipped</span>
-                                                    </td>
+                                                    <td> 20 feburary 2020 </td>
+                                                    <td>  Mens Clothes </td>
+                                                    <td>  13 </td>
+                                                    <td>$63 </td>
+                                                    
                                                 </tr>
-                                                          
+                                                     
+													<tr>
+                                                    <td class="serial">2.</td>
+                                                    <td> 20 feburary 2020 </td>
+                                                    <td>  Women Clothes </td>
+                                                    <td>  15 </td>
+                                                    <td>$69 </td>
+                                                    
+                                                </tr>
+
+													<tr>
+                                                    <td class="serial">3.</td>
+                                                    <td> 20 feburary 2020 </td>
+                                                    <td>  Kids Clothes </td>
+                                                    <td>  10 </td>
+                                                    <td>$58 </td>
+                                                    
+                                                </tr>														
                                             </tbody>
                                         </table>
                                     </div> <!-- /.table-stats -->
@@ -128,65 +152,62 @@
 
                     </div>
                 </div>
-                <!-- /.orders -->
-                <div class="row">
-                    <div class="col-lg-6">
-                    <div class="card">
+                
+				
+				<div class="orders">
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <div class="card">
                                 <div class="card-body">
-                                    <h4 class="box-title">Daily Order Report(Top 5)</h4>
+                                    <h4 class="box-title">Monthly product reports </h4>
                                 </div>
                                 <div class="card-body--">
-                                    <div class="table-stats ">
+                                    <div class="table-stats">
                                         <table class="table ">
                                             <thead>
                                                 <tr>
                                                     <th class="serial">#</th>
+                                                    <th>Month</th>
                                                     <th>Category</th>
-                                                    <th>sales</th>
+                                                    <th>Number of product sold</th>
+                                                    <th>total revenue</th>
                                                 </tr>
-                                            </thead>
+                                            </thead>         
                                             <tbody>
-                                                <tr>
+                                                 <tr>
                                                     <td class="serial">1.</td>
-                                                    <td>  <span class="client name">Louis Stanley</span> </td>
-                                                    <td class="serial"> #6574 </td>
+                                                    <td>  feburary 2020 </td>
+                                                    <td>  Mens Clothes </td>
+                                                    <td>  139 </td>
+                                                    <td>$551 </td>
+                                                    
                                                 </tr>
-                                                                                              
-                                               
-                                               
+                                                     
+													<tr>
+                                                    <td class="serial">2.</td>
+                                                    <td>  feburary 2020 </td>
+                                                    <td>  Women Clothes </td>
+                                                    <td>  156 </td>
+                                                    <td>$604 </td>
+                                                    
+                                                </tr>
+
+													<tr>
+                                                    <td class="serial">3.</td>
+                                                    <td>  feburary 2020 </td>
+                                                    <td>  Kids Clothes </td>
+                                                    <td>  104 </td>
+                                                    <td>$453 </td>
+                                                    
+                                                </tr>			
+                                                          
                                             </tbody>
                                         </table>
                                     </div> <!-- /.table-stats -->
                                 </div>
                             </div> <!-- /.card -->
-                    </div>
+                        </div>  <!-- /.col-lg-8 -->
 
-                    <div class="col-lg-6">
-                    <div class="card">
-                                <div class="card-body">
-                                    <h4 class="box-title">Monthly Order Report(Top 5) </h4>
-                                </div>
-                                <div class="card-body--">
-                                    <div class="table-stats ">
-                                        <table class="table ">
-                                            <thead>
-                                                <tr>
-                                                    <th class="serial">#</th>
-                                                    <th>Category</th>
-                                                    <th>sales</th>
-                                                </tr>
-                                            </thead>
-                                            <tbody>
-                                                <tr>
-                                                    <td class="serial">1.</td>
-                                                    <td>  <span class="client name">Louis Stanley</span> </td>
-                                                    <td class="serial"> #5469 </td>
-                                                 </tr>
-											</tbody>
-                                        </table>                      
-                                    </div> <!-- /.table-stats -->
-                                </div>
-                            </div> <!-- /.card -->
                     </div>
                 </div>
             </div>
